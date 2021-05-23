4b6a85a5ba08d13b895148eb7d344214cd8fa2f6
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-05-23 12:36:12 +0300

    Added Student with fields. Version 1.0

---
+++ src/mp4/Student.java
@@ -0,0 +1,13 @@
+package mp4;
+
+public class Student {
+    private String firstName;
+    private String secondName;
+    private String numerS;
+    private int currentSemestrNumber;
+    private int quantityITN;
+    
+    public final static int minimalCurrentSemestrNumber = 0;
+    public final static int minimalQuantityITN = 0;
+    
+}
