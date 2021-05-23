bd01121f642487f474f3e15786ce5a21f76cfb8d
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-05-23 12:55:36 +0300

    Added in Student gets&sets. Version 1.1

--- src/mp4/Student.java
+++ src/mp4/Student.java
@@ -3,11 +3,70 @@ package mp4;
 public class Student {
     private String firstName;
     private String secondName;
-    private String numerS;
+    private String numerS; // UNIQUE !!!!!
     private int currentSemestrNumber;
     private int quantityITN;
     
     public final static int minimalCurrentSemestrNumber = 0;
     public final static int minimalQuantityITN = 0;
+
+    public Student(String firstName, String secondName, String numerS, int currentSemestrNumber, int quantityITN) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.numerS = numerS;
+        this.currentSemestrNumber = currentSemestrNumber;
+        this.quantityITN = quantityITN;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getNumerS() {
+        return numerS;
+    }
+
+    public void setNumerS(String numerS) {
+        this.numerS = numerS;
+    }
+
+    public int getCurrentSemestrNumber() {
+        return currentSemestrNumber;
+    }
+
+    public void setCurrentSemestrNumber(int currentSemestrNumber) throws Exception {
+        if(currentSemestrNumber < minimalCurrentSemestrNumber){
+            throw new Exception(String.format("Current semestr can't be less then %d", minimalCurrentSemestrNumber));
+        }
+        this.currentSemestrNumber = currentSemestrNumber;
+    }
+
+    public void incrementByOneCurrentSemestrNumber() throws Exception {
+        int semestrNumber = getCurrentSemestrNumber();
+        semestrNumber++;
+        setCurrentSemestrNumber(semestrNumber);
+    }
     
+    public int getQuantityITN() {
+        return quantityITN;
+    }
+
+    public void setQuantityITN(int quantityITN) throws Exception {
+        if(quantityITN < minimalQuantityITN){
+            throw new Exception(String.format("Quantity ITNs can't be less then %d", minimalQuantityITN));
+        }
+        this.quantityITN = quantityITN;
+    }    
 }
